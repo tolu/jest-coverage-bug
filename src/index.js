b7d602fde4701eeca2793e598b7942124126777d
Tobias Lundin <tobias.lundin@gmail.com>
2017-05-11 19:55:43 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 
 
+export const foo = (someValue) => {
+    return `You provided ${someValue}`;
+}
+
+
 export default () => {
     return new Promise((resolve) => {
         setTimeout(() => resolve('ok!'), 500);
